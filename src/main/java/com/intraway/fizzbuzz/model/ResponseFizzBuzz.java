22e076198bdd4d841efe7a27f805c0364e995f33
Wilmer <killerwilmer@misena.edu.co>
2020-02-03 10:00:46 -0500

    add new attributes for response

---
+++ src/main/java/com/intraway/fizzbuzz/model/ResponseFizzBuzz.java
@@ -0,0 +1,91 @@
+package com.intraway.fizzbuzz.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ResponseFizzBuzz {
+
+  private long code;
+  private String list;
+  private String description;
+  private long timestamp;
+  private Integer status;
+  private String error;
+  private String exception;
+  private String message;
+  private String path;
+
+  public ResponseFizzBuzz() {}
+
+  public long getCode() {
+    return code;
+  }
+
+  public String getList() {
+    return list;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setCode(long code) {
+    this.code = code;
+  }
+
+  public void setList(String list) {
+    this.list = list;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public long getTimestamp() {
+    return timestamp;
+  }
+
+  public void setTimestamp(long timestamp) {
+    this.timestamp = timestamp;
+  }
+
+  public Integer getStatus() {
+    return status;
+  }
+
+  public void setStatus(Integer status) {
+    this.status = status;
+  }
+
+  public String getError() {
+    return error;
+  }
+
+  public void setError(String error) {
+    this.error = error;
+  }
+
+  public String getException() {
+    return exception;
+  }
+
+  public void setException(String exception) {
+    this.exception = exception;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
+  public String getPath() {
+    return path;
+  }
+
+  public void setPath(String path) {
+    this.path = path;
+  }
+}
