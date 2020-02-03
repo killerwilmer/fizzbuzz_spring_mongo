acd4534849f65a482ef60e4547ba0a7db14afe27
Wilmer <killerwilmer@misena.edu.co>
2020-02-03 10:00:08 -0500

    Add constants for endpoint and url

---
+++ src/main/java/com/intraway/fizzbuzz/util/Constants.java
@@ -0,0 +1,7 @@
+package com.intraway.fizzbuzz.util;
+
+public class Constants {
+
+  public static final String API_URL = "/intraway/api/";
+  public static final String END_POINT_fIZZ_BUZZ = "fizzbuzz/";
+}
